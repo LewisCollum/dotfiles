152d09ef514b51ea5c7c3063d20e165a9fd4c64f
LewisCollum <lewiscollum4@gmail.com>
2019-10-27 11:57:43 -0400

    fixed emacs not recognizing custom file.

--- emacs/init.el
+++ emacs/init.el
@@ -12,19 +12,5 @@
 (load "~/.emacs.d/code/cpp.el")
 (load "~/.emacs.d/code/latex.el")
 
-(setq bashGeneratedCustom "~/.emacs.d/custom.el")
-(load bashGeneratedCustom)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (org-bullets package-build shut-up epl git commander f dash s))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
