3c5837bfed32cbe728853a8199c8e5eeeb79477c
LewisCollum <lewiscollum4@gmail.com>
2019-10-31 23:45:23 -0400

    Edited org-latex-minted-langs so 'spice' exports as 'text' for latex minted.

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,6 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("75c4727090df6bfdc73c62babb56368cd19df98be903969fe89b9507da45974e" default)))
+ '(org-latex-minted-langs
+   (quote
+    ((emacs-lisp "common-lisp")
+     (cc "c++")
+     (cperl "perl")
+     (shell-script "bash")
+     (caml "ocaml")
+     (spice "text"))))
  '(package-selected-packages (quote (package-build shut-up epl git commander f dash s))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
