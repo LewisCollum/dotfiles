b972ce9fe81a780a32dd01757c5c7aa53cac3c94
LewisCollum <lewiscollum4@gmail.com>
2019-10-31 23:42:08 -0400

    Removed comments.

--- emacs/code/cpp.el
+++ emacs/code/cpp.el
@@ -1,11 +1,12 @@
-(c-add-style "normal-tab-style"
-             '((indent-tabs-mode . nil); use spaces rather than tabs
-               (c-basic-offset . 4); indent by four spaces
-               (c-offsets-alist . ((inline-open . 0); custom indentation rules
-                                   (brace-list-open . 0)
-                                   (statement-case-open . +)))))
+(c-add-style
+ "normal-tab-style"
+ '((indent-tabs-mode . nil)
+   (c-basic-offset . 4)
+   (c-offsets-alist . ((inline-open . 0)
+                       (brace-list-open . 0)
+                       (statement-case-open . +)))))
 
 (defun my-c++-mode-hook ()
-  (c-set-style "normal-tab-style"))        ; use my-style defined above
+  (c-set-style "normal-tab-style"))
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
