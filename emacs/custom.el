00f056272116c4554d7a531c521bc9c7575c387a
LewisCollum <lewiscollum4@gmail.com>
2019-11-10 14:36:38 -0500

    Added packages to install.

--- emacs/custom.el
+++ emacs/custom.el
@@ -14,7 +14,9 @@
      (shell-script "bash")
      (caml "ocaml")
      (spice "text"))))
- '(package-selected-packages (quote (package-build shut-up epl git commander f dash s))))
+ '(package-selected-packages
+   (quote
+    (magit arduino-mode package-build shut-up epl git commander f dash s))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
