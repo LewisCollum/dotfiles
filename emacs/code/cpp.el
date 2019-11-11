453e77a6571b905818ae56aa30918d263af83719
LewisCollum <lewiscollum4@gmail.com>
2019-11-10 22:31:32 -0500

    For C++, fix lambda indentation and remove indentation before {

--- emacs/code/cpp.el
+++ emacs/code/cpp.el
@@ -4,7 +4,9 @@
    (c-basic-offset . 4)
    (c-offsets-alist . ((inline-open . 0)
                        (brace-list-open . 0)
-                       (statement-case-open . +)))))
+                       (statement-case-open . +)
+                       (inlambda . 0) ; no extra indent for lambda
+                       (block-open . 0))))) ; no space before {
 
 (defun my-c++-mode-hook ()
   (c-set-style "normal-tab-style"))
