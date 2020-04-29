9b522416f990f91eb4e3c57276f491f87f9d8eda
LewisCollum <lewiscollum4@gmail.com>
2020-04-29 14:44:54 -0400

    Switched to dejavu font

--- emacs/text.el
+++ emacs/text.el
@@ -1,5 +1,5 @@
 (progn(setq-default indent-tabs-mode nil))
 (setq-default tab-width 4)
-(set-default-font "Inconsolata-16")
+(set-default-font "DejaVu Sans Mono-16")
 (global-visual-line-mode t)
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
