39eb5e6dfe9008636e71b3ba13810f8d94f7d4f1
LewisCollum <lewiscollum4@gmail.com>
2020-04-29 14:45:58 -0400

    support ieeetran latex class

--- emacs/code/latex.el
+++ emacs/code/latex.el
@@ -1,1 +1,9 @@
 (plist-put org-format-latex-options :scale 2.5)
+
+(add-to-list 'org-latex-classes
+             '("IEEEtran" "\\documentclass[10pt]{IEEEtran}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
