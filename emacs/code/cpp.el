89b7e4ae3ba34d1a4dc7230b4f6d5fb5e5b965db
LewisCollum <lewiscollum4@gmail.com>
2020-07-16 16:09:28 +0000

    4 spaces for cmake indent.

--- emacs/code/cpp.el
+++ emacs/code/cpp.el
@@ -1,7 +1,10 @@
+(defconst indentSpaces 4)
+
+(setq cmake-tab-width indentSpaces)
 (c-add-style
  "normal-tab-style"
  '((indent-tabs-mode . nil)
-   (c-basic-offset . 4)
+   (c-basic-offset . indentSpaces)
    (c-offsets-alist . ((inline-open . 0)
                        (brace-list-open . 0)
                        (statement-case-open . +)
