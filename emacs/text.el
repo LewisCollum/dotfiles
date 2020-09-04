a16de5c3aa073fb4eb0adf52a7d1333afa0741e0
LewisCollum <lewiscollum4@gmail.com>
2020-09-04 00:37:09 -0400

    Updated emacs to emacs-27.1-3

--- emacs/text.el
+++ emacs/text.el
@@ -1,6 +1,9 @@
 (progn(setq-default indent-tabs-mode nil))
 (setq-default tab-width 4)
-(set-default-font "DejaVu Sans Mono-16")
+
+(add-to-list 'default-frame-alist
+             '(font . "DejaVu Sans Mono-14"))
+
 (global-visual-line-mode t)
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
