7b29abc2882acf52c4083be6feca4c6796c9c646
LewisCollum <lewiscollum4@gmail.com>
2021-01-13 20:34:40 -0500

    Changes from work

--- emacs/keys.el
+++ emacs/keys.el
@@ -1,2 +1,2 @@
-(global-set-key (kbd "C-x C-a") 'previous-buffer)
+(global-set-key (kbd "C-x C-j") 'previous-buffer)
 (global-set-key (kbd "C-x C-k") 'next-buffer)
--- emacs/text.el
+++ emacs/text.el
@@ -2,7 +2,7 @@
 (setq-default tab-width 4)
 
 (add-to-list 'default-frame-alist
-             '(font . "DejaVu Sans Mono-14"))
+             '(font . "DejaVu Sans Mono-12"))
 
 (global-visual-line-mode t)
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
