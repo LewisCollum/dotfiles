c4d85371e5b562082ac13c2a087e7f1c3174bf19
LewisCollum <lewiscollum4@gmail.com>
2022-11-30 15:43:09 -0500

    Removed emacs from dotfiles, since I have a separate repo for it

--- emacs/text.el
+++
@@ -1,9 +0,0 @@
-(progn(setq-default indent-tabs-mode nil))
-(setq-default tab-width 4)
-
-(add-to-list 'default-frame-alist
-             '(font . "DejaVu Sans Mono-12"))
-
-(global-visual-line-mode t)
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-
